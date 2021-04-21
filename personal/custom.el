aa77038f4a48c153302766a2e0379e40184d6f2c
lyderichti59 <lyderic.dutillieux@origenial.fr>
2021-04-21 20:10:58 +0200

    Added some UI libraries to enhance IVY and general looking
    Signed-off-by: lyderichti59 <lyderic.dutillieux@origenial.fr>

--- personal/custom.el
+++ personal/custom.el
@@ -90,11 +90,16 @@ Captured On: %U")))
      (sequence "TODO(t)" "NEXT(n)" "WIP(w)" "DONE(d)")))
  '(org-use-fast-todo-selection 'expert)
  '(package-selected-packages
-   '(nix-mode rjsx-mode php-mode tldr helm-projectile helm async avy bind-key dash dash-functional gh git-commit ht inflections logito lsp-mode markdown-mode marshal multiple-cursors pcache prescient transient with-editor rust-mode org-roam visual-regexp-steroids visual-regexp all-the-icons-dired all-the-icons-ivy-rich all-the-icons neotree tagedit scss-mode anakondo docker-compose-mode aggressive-indent pretty-symbols fira-code-mode use-package ta clojure-snippets afternoon-theme dracula-theme flycheck-clj-kondo command-log-mode clj-refactor cider clojure-mode-extra-font-locking clojure-mode yaml-mode web-mode lsp-ui json-mode rainbow-mode elisp-slime-nav rainbow-delimiters company counsel swiper ivy-prescient ivy exec-path-from-shell zop-to-char zenburn-theme which-key volatile-highlights undo-tree super-save smartrep smartparens operate-on-number nlinum move-text magit projectile imenu-anywhere hl-todo guru-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region epl editorconfig easy-kill diminish diff-hl discover-my-major crux browse-kill-ring anzu ag ace-window))
+   '(counsel-projectile helpful doom-modeline lsp-treemacs nix-mode rjsx-mode php-mode tldr helm-projectile helm async avy bind-key dash dash-functional gh git-commit ht inflections logito lsp-mode markdown-mode marshal multiple-cursors pcache prescient transient with-editor rust-mode org-roam visual-regexp-steroids visual-regexp all-the-icons-dired all-the-icons-ivy-rich all-the-icons neotree tagedit scss-mode anakondo docker-compose-mode aggressive-indent pretty-symbols fira-code-mode use-package ta clojure-snippets afternoon-theme dracula-theme flycheck-clj-kondo command-log-mode clj-refactor cider clojure-mode-extra-font-locking clojure-mode yaml-mode web-mode lsp-ui json-mode rainbow-mode elisp-slime-nav rainbow-delimiters company counsel swiper ivy-prescient ivy exec-path-from-shell zop-to-char zenburn-theme which-key volatile-highlights undo-tree super-save smartrep smartparens operate-on-number move-text magit projectile imenu-anywhere hl-todo guru-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region epl editorconfig easy-kill diminish diff-hl discover-my-major crux browse-kill-ring anzu ag ace-window))
  '(prelude-flyspell nil)
  '(prelude-theme 'dracula-theme)
+ '(projectile-globally-ignored-directories
+   '(".idea" ".vscode" ".ensime_cache" ".eunit" ".git" ".hg" ".fslckout" "_FOSSIL_" ".bzr" "_darcs" ".tox" ".svn" ".stack-work" ".ccls-cache" ".cache" ".clangd" "node_modules" ".next"))
  '(safe-local-variable-values
-   '((cider-shadow-watched-builds "app")
+   '((cider-preferred-build-tool . shadow-cljs)
+     (cider-shadow-default-options . "app")
+     (cider-default-cljs-repl . shadow)
+     (cider-shadow-watched-builds "app")
      (eval font-lock-add-keywords nil
            `((,(concat "("
                        (regexp-opt
@@ -102,7 +107,6 @@ Captured On: %U")))
                         t)
                        "\\_>")
               1 'font-lock-variable-name-face)))))
- '(server-after-make-frame-hook '(setup-neo-theme setup-firacode-font))
  '(size-indication-mode t)
  '(sp-base-key-bindings 'paredit)
  '(sp-override-key-bindings
