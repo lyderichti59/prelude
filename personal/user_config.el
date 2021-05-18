9805875311663c1c3382340f67f57a633fc709ad
Lyderic Dutillieux <lyderic.dutillieux@origenial.fr>
2021-05-18 08:28:58 +0200

    Added centaur-tabs configuration
    Signed-off-by: Lyderic Dutillieux <lyderic.dutillieux@origenial.fr>

--- personal/user_config.el
+++ personal/user_config.el
@@ -44,6 +44,65 @@
   (doom-modeline-mode 1)
   (add-hook 'after-make-frame-functions 'setup-doom-modeline-icons))
 
+;; Adding tabs management
+(use-package centaur-tabs
+  :demand
+  :config
+  (setq centaur-tabs-style "bar"
+        centaur-tabs-set-icons t
+        centaur-tabs-height 32
+        centaur-tabs-set-bar 'over
+        centaur-tabs-set-modified-marker t
+        centaur-tabs-cycle-scope 'tabs)
+  (centaur-tabs-mode t)
+  (centaur-tabs-group-by-projectile-project)
+  (defun centaur-tabs-buffer-groups ()
+     "`centaur-tabs-buffer-groups' control buffers' group rules.
+ Group centaur-tabs with mode if buffer is derived from `eshell-mode' `emacs-lisp-mode' `dired-mode' `org-mode' `magit-mode'.
+ All buffer name start with * will group to \"Emacs\".
+ Other buffer group by `centaur-tabs-get-group-name' with project name."
+   (list
+    (cond
+;; ((not (eq (file-remote-p (buffer-file-name)) nil))
+;; "Remote")
+    ((or (string-equal "*" (substring (buffer-name) 0 1))
+         (memq major-mode '(magit-process-mode
+            magit-status-mode
+            magit-diff-mode
+            magit-log-mode
+            magit-file-mode
+            magit-blob-mode
+            magit-blame-mode)))
+     "Emacs")
+    ((derived-mode-p 'prog-mode)
+     "Editing")
+    ((derived-mode-p 'dired-mode)
+     "Dired")
+    ((memq major-mode '(helpful-mode
+                help-mode))
+     "Help")
+    ((memq major-mode '(org-mode
+                org-agenda-clockreport-mode
+                org-src-mode
+                org-agenda-mode
+                org-beamer-mode
+                org-indent-mode
+                org-bullets-mode
+                org-cdlatex-mode
+                org-agenda-log-mode
+                diary-mode))
+     "OrgMode")
+    (t
+     (centaur-tabs-get-group-name (current-buffer))))))
+  :hook
+  (dired-mode . centaur-tabs-local-mode)
+  (term-mode . centaur-tabs-local-mode)
+  (calendar-mode . centaur-tabs-local-mode)
+  (org-agenda-mode . centaur-tabs-local-mode)
+  (helpful-mode . centaur-tabs-local-mode)
+  :bind
+  ("C-<prior>" . centaur-tabs-backward)
+  ("C-<next>" . centaur-tabs-forward))
 
 ;; Having short description beside commands when `M-x`
 (use-package ivy-rich
