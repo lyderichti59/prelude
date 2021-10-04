48fa96defc8ef9b1658e58f55540c5f3aa8da0d7
Lyderic Dutillieux <lyderic.dutillieux@origenial.fr>
2021-10-04 10:15:12 +0200

    Add some CSV features
    Signed-off-by: Lyderic Dutillieux <lyderic.dutillieux@origenial.fr>

---
+++ personal/csv_highlight.el
@@ -0,0 +1,17 @@
+(require 'cl)
+(require 'color)
+
+(defun csv-highlight (&optional separator)
+  (interactive (list (when current-prefix-arg (read-char "Separator: "))))
+  (font-lock-mode 1)
+  (let* ((separator (or separator ?\,))
+         (n (count-matches (string separator) (point-at-bol) (point-at-eol)))
+         (colors (loop for i from 0 to 1.0 by (/ 2.0 n)
+                       collect (apply #'color-rgb-to-hex
+                                      (color-hsl-to-rgb i 0.3 0.5)))))
+    (loop for i from 2 to n by 2
+          for c in colors
+          for r = (format "^\\([^%c\n]+%c\\)\\{%d\\}" separator separator i)
+          do (font-lock-add-keywords nil `((,r (1 '(face (:foreground ,c)))))))))
+
+(provide 'csv-highlight)
